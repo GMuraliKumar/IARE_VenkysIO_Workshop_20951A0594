e08e974c249bd570ecebd684ab1309b488e4a385
GMuraliKumar <126311254+GMuraliKumar@users.noreply.github.com>
2023-02-25 11:50:53 +0530

    Delete grosssalary.py

--- grosssalary.py
+++
@@ -1,7 +0,0 @@
-gr=float(input("Enter Gross Salary : "))
-fd=float(input("Enter Federal Deduction : "))
-sd=float(input("Enter State Deduction: "))
-pe=float(input("Enter Pension Pecentage : "))
-nw=float(input("Enter Number of Weeks : "))
-tot=(fd+sd+pe)*(gr/100)
-print("Total Salary : ",4*(gr-tot))
